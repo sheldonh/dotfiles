7b0b1be9d33b4153b07b649e924160ff73b9cb9b
Sheldon Hearn <sheldonh@starjuice.net>
2012-04-18 11:06:43 +0200

    Install vagrant

--- puppet/manifests/axl.pp
+++ puppet/manifests/axl.pp
@@ -3,9 +3,11 @@ Exec {
 }
 
 include kerberos
+include vagrant
 
 package { 'git': ensure => installed }
 package { 'vim-enhanced': ensure => installed }
+package { 'xchat': ensure => installed }
 
 user { 'sheldonh': }
 users::rvm { 'sheldonh': }

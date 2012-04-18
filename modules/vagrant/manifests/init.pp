c6dd84fd4c1c2b05fd59bfbce6643fd1c77c42d8
Sheldon Hearn <sheldonh@starjuice.net>
2012-04-18 19:25:40 +0200

    Don't provision vagrant on virtual machine

--- modules/vagrant/manifests/init.pp
+++ modules/vagrant/manifests/init.pp
@@ -1,12 +1,16 @@
 class vagrant {
 
-  include virtualbox
+  if $::virtual == 'physical' {
+    include virtualbox
 
-  # As of puppet-2.7.13, the yum provider doesn't support the source package parameter.
-  exec { 'install-vagrant':
-    command => 'yum install -y http://files.vagrantup.com/packages/41445466ee4d376601fd8d0c6a5e3af61d32f131/vagrant_1.0.2_x86_64.rpm',
-    creates => '/opt/vagrant/bin/vagrant',
-    require => Class['virtualbox'],
+    # As of puppet-2.7.13, the yum provider doesn't support the source package parameter.
+    exec { 'install-vagrant':
+      command => 'yum install -y http://files.vagrantup.com/packages/41445466ee4d376601fd8d0c6a5e3af61d32f131/vagrant_1.0.2_x86_64.rpm',
+      creates => '/opt/vagrant/bin/vagrant',
+      require => Class['virtualbox'],
+    }
+  } else {
+    notice("Refusing to provision vagrant on virtual platform '$::virtual'")
   }
 
 }

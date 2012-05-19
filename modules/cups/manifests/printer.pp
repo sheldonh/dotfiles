68f4547f75ed952d955c9acf07d8a06bd84f7a42
Sheldon Hearn <sheldonh@starjuice.net>
2012-05-19 16:08:05 +0200

    Support printer driver as URL

--- modules/cups/manifests/printer.pp
+++ modules/cups/manifests/printer.pp
@@ -1,4 +1,4 @@
-define cups::printer($driver, $driver_package = false, $address, $cups_default = false) {
+define cups::printer($driver = false, $driver_package = false, $address, $cups_default = false) {
 
   if $driver_package {
     if !defined( Package[$driver_package] ) { package { $driver_package: ensure => installed } }
@@ -6,10 +6,26 @@ define cups::printer($driver, $driver_package = false, $address, $cups_default =
     Package[$driver_package] -> Exec["add-printer-$name"]
   }
 
-  if $cups_default {
-    $command = "lpadmin -p $name -v socket://$address/ -P $driver -E && lpadmin -d $name"
+  if $driver {
+    if $driver =~ /^https?:\/\/.+\/([^\/]+)$/ {
+      $driver_filename = "/usr/src/$1"
+      exec { "fetch-$driver":
+        command => "curl -L -o $driver_filename $driver",
+        creates => $driver_filename,
+        before  => Exec["add-printer-$name"],
+      }
+    } else {
+      $driver_filename = $driver
+    }
+    $driver_opts = "-P $driver_filename"
   } else {
-    $command = "lpadmin -p $name -v socket://$address/ -P $driver -E"
+    $driver_opts = ''
+  }
+
+  if $cups_default {
+    $command = "lpadmin -p $name -v socket://$address/ $driver_opts -E && lpadmin -d $name"
+  } else {
+    $command = "lpadmin -p $name -v socket://$address/ $driver_opts -E"
   }
 
   exec { "add-printer-$name":

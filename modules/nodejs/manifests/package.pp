65f8457b71620fb5d0db91d8b19a602f167eb507
Sheldon Hearn <sheldonh@starjuice.net>
2012-05-27 23:12:29 +0200

    Support git urls

--- modules/nodejs/manifests/package.pp
+++ modules/nodejs/manifests/package.pp
@@ -1,18 +1,27 @@
-define nodejs::package($ensure = 'installed') {
+define nodejs::package($ensure = 'installed', $source = false) {
 
   include nodejs::npm
+
+  if $source {
+    $target = $source
+    if $target =~ /^git:\/\// {
+      if !defined( Package['git'] ) { package { 'git': ensure => installed } }
+    }
+  } else {
+    $target = $name
+  }
 
   case $ensure {
     'absent': {
       exec { "npm-uninstall-$name":
-        command => "npm uninstall -g $name",
+        command => "npm uninstall -g $target",
         onlyif  => "npm ls -g 2>/dev/null | grep ' ${name}@'",
         require => Class['nodejs::npm'],
       }
     }
     'installed': {
       exec { "npm-install-$name":
-        command => "npm install -g $name",
+        command => "npm install -g $target",
         unless  => "npm ls -g 2>/dev/null | grep ' ${name}@'",
         require => Class['nodejs::npm'],
       }

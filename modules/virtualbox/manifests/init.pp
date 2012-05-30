46fde75f6e3fa475ecc6056dff4428da57d1fde5
Sheldon Hearn <sheldonh@starjuice.net>
2012-05-30 15:16:53 +0200

    Fixes for Fedora 17

--- modules/virtualbox/manifests/init.pp
+++ modules/virtualbox/manifests/init.pp
@@ -16,6 +16,13 @@ class virtualbox {
   if !defined( Package['dkms'] )         { package { 'dkms': ensure => installed } }
   if !defined( Package['kernel-devel'] ) { package { 'kernel-devel': ensure => installed } }
 
+  # https://bugzilla.redhat.com/bugzilla/show_bug.cgi?id=790521
+  exec { 'fix-redhat-bug-790521':
+    command => 'mv /usr/sbin/dkms.old /usr/sbin/dkms',
+    creates => '/usr/sbin/dkms',
+    before  => Exec['setup-virtualbox'],
+  }
+
   exec { 'setup-virtualbox':
     command     => 'if /etc/init.d/vboxdrv setup 2>&1 | grep -q FAILED; then false; else true; fi',
     onlyif      => 'vboxmanage --version 2>&1 | grep -q WARNING',

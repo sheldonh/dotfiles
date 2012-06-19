c9e5eac0d70b2844041e1211ac3df2d2859944fb
Sheldon Hearn <sheldonh@starjuice.net>
2012-06-19 15:02:04 +0200

    Dkms bugfix means workaround isn't required at all

--- modules/virtualbox/manifests/init.pp
+++ modules/virtualbox/manifests/init.pp
@@ -16,15 +16,6 @@ class virtualbox {
   if !defined( Package['dkms'] )         { package { 'dkms': ensure => installed } }
   if !defined( Package['kernel-devel'] ) { package { 'kernel-devel': ensure => installed } }
 
-  # https://bugzilla.redhat.com/bugzilla/show_bug.cgi?id=790521
-  # Fixed in Fedora 17
-  exec { 'fix-redhat-bug-790521':
-    command => 'mv /usr/sbin/dkms.old /usr/sbin/dkms',
-    creates => '/usr/sbin/dkms',
-    onlyif  => '[ -e /usr/sbin/dkms.old ]',
-    before  => Exec['setup-virtualbox'],
-  }
-
   exec { 'setup-virtualbox':
     command     => 'if /etc/init.d/vboxdrv setup 2>&1 | grep -q FAILED; then false; else true; fi',
     onlyif      => 'vboxmanage --version 2>&1 | grep -q WARNING',

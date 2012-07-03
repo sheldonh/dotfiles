4a4d48e6cc3c144420f02eac2afcba7b27d429c6
Sheldon Hearn <sheldonh@starjuice.net>
2012-07-03 09:40:03 +0200

    Give up and install slapd for aaapi

--- modules/hetzner/manifests/aaapi.pp
+++ modules/hetzner/manifests/aaapi.pp
@@ -3,5 +3,8 @@ class hetzner::aaapi {
   if ! defined( Package['libxml2-devel'] )    { package { 'libxml2-devel': ensure => installed } }
   if ! defined( Package['libxslt-devel'] )    { package { 'libxslt-devel': ensure => installed } }
   if ! defined( Package['openldap-devel'] )    { package { 'openldap-devel': ensure => installed } }
+  if ! defined( Package['openldap-servers'] )    { package { 'openldap-servers': ensure => installed } }
+
+  if !defined( Service['slapd'] ) { service { 'slapd': enable => false } }
 
 }

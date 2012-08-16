76a53e73b7407b8215d0d8004f2b8f118a1c14bd
Sheldon Hearn <sheldonh@starjuice.net>
2012-08-16 10:34:53 +0200

    Prepare parent directories of dotfiles

--- modules/users/manifests/dotfile.pp
+++ modules/users/manifests/dotfile.pp
@@ -1,11 +1,26 @@
 define users::dotfile($user, $home = "/home/$user", $mode = '0644') {
 
-  file { $name:
-    path   => "$home/.$name",
-    owner  => $user,
-    group  => $user,
-    mode   => $mode,
-    source => ["puppet:///modules/users/$user/dotfiles/$name.enc", "puppet:///modules/users/$user/dotfiles/$name"],
+  $dotfile = $name
+
+  $dirname = regsubst($dotfile, '^(.+)/([^/]+)$', '\1')
+  if $dirname != $dotfile {
+    exec { "prepare-dotfile-tree-$user-$name":
+      command => "mkdir -p $home/.$dirname",
+      creates => "$home/.$dirname",
+      provider => shell,
+      user     => $user,
+      group    => $user,
+      before   => File["$user-$dotfile"],
+    }
+  }
+
+  file { "$user-$dotfile":
+    path    => "$home/.$dotfile",
+    owner   => $user,
+    group   => $user,
+    mode    => $mode,
+    source  => ["puppet:///modules/users/$user/dotfiles/$dotfile.enc",
+                "puppet:///modules/users/$user/dotfiles/$dotfile"],
   }
 
 }

7dbca4bc65c29352778bfe0698607c926679efc1
Sheldon Hearn <sheldonh@starjuice.net>
2012-08-28 15:41:36 +0200

    Add openldap-clients for aaapi
    Needed to set up admin user as part of AAAPI LDAP_HOWTO

--- modules/hetzner/manifests/aaapi.pp
+++ modules/hetzner/manifests/aaapi.pp
@@ -2,8 +2,9 @@ class hetzner::aaapi {
 
   if ! defined( Package['libxml2-devel'] )    { package { 'libxml2-devel': ensure => installed } }
   if ! defined( Package['libxslt-devel'] )    { package { 'libxslt-devel': ensure => installed } }
-  if ! defined( Package['openldap-devel'] )    { package { 'openldap-devel': ensure => installed } }
-  if ! defined( Package['openldap-servers'] )    { package { 'openldap-servers': ensure => installed } }
+  if ! defined( Package['openldap-clients'] ) { package { 'openldap-clients': ensure => installed } }
+  if ! defined( Package['openldap-devel'] )   { package { 'openldap-devel': ensure => installed } }
+  if ! defined( Package['openldap-servers'] ) { package { 'openldap-servers': ensure => installed } }
 
   if !defined( Service['slapd'] ) { service { 'slapd': enable => false } }
 

684c828e310e03cb6a9b0f2c269ce8e777bca7cc
Sheldon Hearn <sheldonh@starjuice.net>
2012-09-18 15:24:29 +0200

    Install phpunit for konsoleh testing

--- modules/hetzner/manifests/konsoleh.pp
+++ modules/hetzner/manifests/konsoleh.pp
@@ -4,6 +4,7 @@ class hetzner::konsoleh {
   if ! defined( Package['libxslt-devel'] )          { package { 'libxslt-devel': ensure => installed } }
   if ! defined( Package['mysql-devel'] )            { package { 'mysql-devel': ensure => installed } }
   if ! defined( Package['openldap-devel'] )         { package { 'openldap-devel': ensure => installed } }
+  if ! defined( Package['php-phpunit-PHPUnit'] )    { package { 'php-phpunit-PHPUnit': ensure => installed } }
   if ! defined( Package['postgresql-devel'] )       { package { 'postgresql-devel': ensure => installed } }
   if ! defined( Package['qtwebkit-devel'] )         { package { 'qtwebkit-devel': ensure => installed } }
   if ! defined( Package['xorg-x11-server-Xvfb'] )   { package { 'xorg-x11-server-Xvfb': ensure => installed } }

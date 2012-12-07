9ec4f794d63d5be1dc8ff3ade205b89f6f387457
Sheldon Hearn <sheldonh@starjuice.net>
2012-12-07 12:15:08 +0200

    Fix virtualbox on fc18

--- modules/virtualbox/manifests/init.pp
+++ modules/virtualbox/manifests/init.pp
@@ -1,26 +1,11 @@
 class virtualbox {
 
-  file { '/etc/yum.repos.d/virtualbox.repo':
-    owner => root,
-    group => root,
-    mode  => '0644',
-    source => 'puppet:///modules/virtualbox/virtualbox.repo',
-  }
+  include rpmfusion::free
 
-  package { 'VirtualBox-4.1':
+  package { 'VirtualBox':
     ensure  => installed,
     alias   => 'virtualbox',
-    require => File['/etc/yum.repos.d/virtualbox.repo'],
-  }
-
-  if !defined( Package['dkms'] )         { package { 'dkms': ensure => installed } }
-  if !defined( Package['kernel-devel'] ) { package { 'kernel-devel': ensure => installed } }
-
-  exec { 'setup-virtualbox':
-    command     => 'if /etc/init.d/vboxdrv setup 2>&1 | grep -q FAILED; then false; else true; fi',
-    onlyif      => 'vboxmanage --version 2>&1 | grep -q WARNING',
-    provider    => shell,
-    require     => Package[['virtualbox', 'dkms', 'kernel-devel']],
+    require => Class['rpmfusion::free'],
   }
 
 }

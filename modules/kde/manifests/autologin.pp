dffc3efd2623831939c3a00adff5687c0106a168
Sheldon Hearn <sheldonh@starjuice.net>
2012-12-07 12:15:36 +0200

    Fix KDE autologin on fc18

--- modules/kde/manifests/autologin.pp
+++ modules/kde/manifests/autologin.pp
@@ -3,12 +3,12 @@ class kde::autologin($user) {
   include kde
 
   exec { 'enable-kde-autologin':
-    command => "sed -i -e 's/^AutoLoginEnable=false/AutoLoginEnable=true/' /etc/kde/kdm/kdmrc",
+    command => "sed -i -e 's/^\\#*AutoLoginEnable=.*/AutoLoginEnable=true/' /etc/kde/kdm/kdmrc",
     unless  => "grep '^AutoLoginEnable=true' /etc/kde/kdm/kdmrc",
   }
 
   exec { 'set-kde-autologin-user':
-    command => "sed -i -e 's/^AutoLoginUser=.*/AutoLoginUser=$user/' /etc/kde/kdm/kdmrc",
+    command => "sed -i -e 's/^\\#*AutoLoginUser=.*/AutoLoginUser=$user/' /etc/kde/kdm/kdmrc",
     unless  => "grep '^AutoLoginUser=$user' /etc/kde/kdm/kdmrc",
   }
 

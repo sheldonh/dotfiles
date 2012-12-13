3b5861dabc961f7ee8b169d11d958cc238c27870
Sheldon Hearn <sheldonh@starjuice.net>
2012-12-13 11:46:27 +0200

    Fix vagrant on fc18

--- modules/vagrant/manifests/init.pp
+++ modules/vagrant/manifests/init.pp
@@ -2,10 +2,11 @@ class vagrant {
 
   if $::virtual == 'physical' {
     if !defined( Package['VirtualBox'] ) { package { 'VirtualBox': ensure  => installed } }
+    if !defined( Package['kmod-VirtualBox'] ) { package { 'kmod-VirtualBox': ensure  => installed } }
 
     # As of puppet-2.7.13, the yum provider doesn't support the source package parameter.
     exec { 'install-vagrant':
-      command => 'yum install -y http://files.vagrantup.com/packages/41445466ee4d376601fd8d0c6a5e3af61d32f131/vagrant_1.0.2_x86_64.rpm',
+      command => 'yum install -y http://files.vagrantup.com/packages/be0bc66efc0c5919e92d8b79e973d9911f2a511f/vagrant_1.0.5_x86_64.rpm',
       creates => '/opt/vagrant/bin/vagrant',
       require => Package['VirtualBox'],
     }
